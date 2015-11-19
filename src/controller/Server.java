572ac1810112fb2125244d4c286697a5f2e720ce
Bryon Berryhill <berryhillb@yahoo.com>
2015-11-19 16:04:49 -0700

    server thing

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,10 +1,15 @@
 package controller;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		HttpServer sever = HttpServer.create(new InetSocketAddress(8000),8000);
 	}
 
 }
